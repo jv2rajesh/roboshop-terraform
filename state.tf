5e2dc5ba94c4dc20bc52a8b7cc1984bf900265f4
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-11 18:47:11 +0530

    Terraform

--- state.tf
+++ state.tf
@@ -6,16 +6,3 @@ terraform {
 
 
 
-
-
-
-
-/* https://developer.hashicorp.com/terraform/language/settings/backends/s3 */
-
-/* A backend defines where Terraform stores its state data files.
-
-  Terraform uses persisted state data to keep track of the resources it manages. Most non-trivial Terraform configurations either integrate with Terraform Cloud or use a backend to store state remotely. This lets multiple people access the state data and work together on that collection of infrastructure resources.
-
-*/
-
-/* session-29 */
