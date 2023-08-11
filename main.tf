98e14a0adc821298cdd32b6513cf9450e02f4583
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-11 15:11:33 +0530

    provisioner and module in terraform

--- main.tf
+++ main.tf
@@ -18,4 +18,12 @@ output "test" {
 
 /* https://developer.hashicorp.com/terraform/language/settings/backends/s3 */
 
+
+/*  terraform init -backend-config=env-dev/state.tfvars
+    terraform apply -auto-approve -var-file=env-dev/main.tfvars
+    rm -rf .terraform   (Need to remove .terraform directory before changing from one environment to another environment)
+    terraform init -backend-config=env-prod/state.tfvars
+    terraform apply -auto-approve -var-file=env-prod/main.tfvars
+
+*/
 /* started in session-29 */
