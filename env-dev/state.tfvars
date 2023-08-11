9d750749e393506904a7cc04ab6c0a99e5aaa199
Rajesh Reddy <jv2rajesh@gmail.com>
2023-08-11 18:24:42 +0530

    Terraform

--- env-dev/state.tfvars
+++ env-dev/state.tfvars
@@ -1,3 +1,11 @@
 bucket = "jv2rajesh"
 key    = "dev/terraform.tfstate"
 region = "us-east-1"
+
+
+
+/* terraform.tfstate - is a object name we chose
+
+   Amazon S3 > Buckets > jv2rajesh > dev/
+
+ */
--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ module "components" {
 
 
 
-
-
 /* source - https://developer.hashicorp.com/terraform/language/modules/sources#generic-git-repository */
 
-/*variable "test" {}
+/*
+variable "test" {}
 
 output "test" {
   value = var.test
+
 }*/
 
 /* https://developer.hashicorp.com/terraform/language/settings/backends/s3 */
@@ -28,8 +28,11 @@ output "test" {
 
 /*  terraform init -backend-config=env-dev/state.tfvars
     terraform apply -auto-approve -var-file=env-dev/main.tfvars
+
     rm -rf .terraform   (Need to remove .terraform directory before changing from one environment to another environment)
+
     terraform init -backend-config=env-prod/state.tfvars
     terraform apply -auto-approve -var-file=env-prod/main.tfvars
 */
+
 /* started in session-29 */
